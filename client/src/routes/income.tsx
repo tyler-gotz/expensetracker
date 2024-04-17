d6008f909c7b9500f9d7a9ba8437e16ac903495e
Tyler Gotz <tyler.gotz@teschglobal.com>
2024-04-17 16:17:32 -0500

    mocking up income and expense pages

--- client/src/routes/income.tsx
+++ client/src/routes/income.tsx
@@ -1,8 +1,233 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuItem, DropdownMenuLabel } from '@/components/ui/dropdown-menu'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { MoreHorizontal, Plus } from 'lucide-react'
 
 const Income = () => {
     return (
-        <div>Income</div>
+        <div className='px-4'>
+            <Card>
+                <CardHeader className='flex flex-row justify-between'>
+                    <div>
+                        <CardTitle>Income</CardTitle>
+                        <CardDescription>
+                            Manage your income here
+                        </CardDescription>
+                    </div>
+                    <Button size='icon'>
+                        <Plus className='h-5 w-5' />
+                        <span className='sr-only'>Add Income</span>
+                    </Button>
+                </CardHeader>
+                <CardContent>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Description</TableHead>
+                                <TableHead className="hidden md:table-cell">
+                                    Amount
+                                </TableHead>
+                                <TableHead className="hidden md:table-cell">
+                                    Date & Time
+                                </TableHead>
+                                <TableHead>
+                                    <span className="sr-only">Actions</span>
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell className="font-medium">
+                                    Laser Lemonade Machine
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    $499.99
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    2023-07-12 10:42 AM
+                                </TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button
+                                                aria-haspopup="true"
+                                                size="icon"
+                                                variant="ghost"
+                                            >
+                                                <MoreHorizontal className="h-4 w-4" />
+                                                <span className="sr-only">Toggle menu</span>
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">
+                                    Hypernova Headphones
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    $129.99
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    2023-10-18 03:21 PM
+                                </TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button
+                                                aria-haspopup="true"
+                                                size="icon"
+                                                variant="ghost"
+                                            >
+                                                <MoreHorizontal className="h-4 w-4" />
+                                                <span className="sr-only">Toggle menu</span>
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">
+                                    AeroGlow Desk Lamp
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    $39.99
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    2023-11-29 08:15 AM
+                                </TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button
+                                                aria-haspopup="true"
+                                                size="icon"
+                                                variant="ghost"
+                                            >
+                                                <MoreHorizontal className="h-4 w-4" />
+                                                <span className="sr-only">Toggle menu</span>
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">
+                                    TechTonic Energy Drink
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    $2.99
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    2023-12-25 11:59 PM
+                                </TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button
+                                                aria-haspopup="true"
+                                                size="icon"
+                                                variant="ghost"
+                                            >
+                                                <MoreHorizontal className="h-4 w-4" />
+                                                <span className="sr-only">Toggle menu</span>
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">
+                                    Gamer Gear Pro Controller
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    $59.99
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    2024-01-01 12:00 AM
+                                </TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button
+                                                aria-haspopup="true"
+                                                size="icon"
+                                                variant="ghost"
+                                            >
+                                                <MoreHorizontal className="h-4 w-4" />
+                                                <span className="sr-only">Toggle menu</span>
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="font-medium">
+                                    Luminous VR Headset
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    $199.99
+                                </TableCell>
+                                <TableCell className="hidden md:table-cell">
+                                    2024-02-14 02:14 PM
+                                </TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button
+                                                aria-haspopup="true"
+                                                size="icon"
+                                                variant="ghost"
+                                            >
+                                                <MoreHorizontal className="h-4 w-4" />
+                                                <span className="sr-only">Toggle menu</span>
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </CardContent>
+                <CardFooter>
+                    <div className="text-xs text-muted-foreground">
+                        Showing <strong>1-10</strong> of <strong>32</strong>{" "}
+                        income entries
+                    </div>
+                </CardFooter>
+            </Card>
+        </div>
     )
 }
 
